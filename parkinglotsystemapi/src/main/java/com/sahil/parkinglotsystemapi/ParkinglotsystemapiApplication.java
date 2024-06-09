d26a850467495eecf1e40625b11568ec724cbb56
Sahil Shrivastava <skshrivastava1234@gmail.com>
2024-06-09 19:25:14 +0530

    init project

---
+++ parkinglotsystemapi/src/main/java/com/sahil/parkinglotsystemapi/ParkinglotsystemapiApplication.java
@@ -0,0 +1,13 @@
+package com.sahil.parkinglotsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotsystemapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkinglotsystemapiApplication.class, args);
+    }
+
+}
---
+++ parkinglotsystemapi/src/test/java/com/sahil/parkinglotsystemapi/ParkinglotsystemapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.parkinglotsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotsystemapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
